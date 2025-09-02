9235bd753ccb3822cbef1022ee3b98093cf4ce21
Xiujinhub <xxj17306690937@163.com>
2025-09-02 21:51:39 +0800

    Update main.cpp

--- ImageEnhance/SSE-Retinex/main.cpp
+++ ImageEnhance/SSE-Retinex/main.cpp
@@ -12,8 +12,8 @@ int main()
         return -1;
     }
 
-    std::vector<double> sigma = { 1, 3, 5 };
-    std::vector<double> w(3, 1 / 3.0);
+    std::vector<double> sigma = { 5, 15, 25 };   //sigma参数就是高斯卷积核大小，这也是影响算法效率的一个重要关键因素
+    std::vector<double> w(3, 1 / 3.0); // 这里直接三等均分
     retinex.multi_retinex_color_restoration_process(img, img, sigma, w, 5, 25, 125, 46);
     cv::imshow("retinex_result", img);
     cv::waitKey(0);
